f4b9167816ec9a97883b507538042f3f2a0bf2c4
juliaaoliveeirs <juliadeoliveira.gti@gmail.com>
2026-03-09 14:57:14 +0000

    alteração da porta

--- 1_deploy/exemplo2.py
+++ 1_deploy/exemplo2.py
@@ -49,4 +49,4 @@ if __name__ == "__main__":
         skip_if_exists=True,
         reader=PDFReader()
     )
-    agent_os.serve(app="exemplo2:app", host="0.0.0.0", port=7777, reload=True)
+    agent_os.serve(app="exemplo2:app", host="0.0.0.0", port=10000, reload=True)
